da4e1d80c27561973804cf8b8479fbce7cb658c8
gianniresende <gianniresende@gmail.com>
2021-05-10 14:52:52 -0300

    feat: add method calculate total price to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,18 @@ class Order < ApplicationRecord
 
   enum status: { waiting: 0, delivered: 1}
 
+  before_validation :set_price
+
+  accepts_nested_attributes_for :order_products, allow_destroy: true
+
+  private
+
+  def set_price
+    final_price = 0
+    order_products.each do |op|
+      final_price += op.quantity * op.product.price
+    end
+    self.total_value = final_price + self.restaurant.delivery_tax
+  end
+
 end
